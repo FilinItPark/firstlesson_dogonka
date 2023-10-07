ada2b23b54f0c81e8603a4e76cbe698814e458a8
Ivan <54151565+1ommyS@users.noreply.github.com>
2023-10-07 19:36:08 +0300

    first lesson

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+/**
+ * @author 1ommy
+ * @version 07.10.2023
+ */
+public class Main {
+    public static void main(String[] args) {
+//        int a = 5;
+//        long b = 5l;
+//        double d = 1.7;
+//        float dd = 2.3f;
+//        System.out.println(a);
+//
+//        char e = 'p';
+//        System.out.println(e);
+////        String name = "Ivan";
+////        System.out.println(name.length());
+//
+//        int k = 8 / 4;
+//        int l = 5 / 8; // 0.625
+//
+//        int f1 = 5;
+//        int f2 = 8;
+//        double l1 = (double) f1 / f2;
+//        System.out.println(l1);
+//        System.out.println((int) l1);
+//        double e1 = 5.7;
+//        double e2 = 6.3;
+//        System.out.println(e1 * e2);
+//
+//        String result = "Hello " + "world";
+//        System.out.println(result);
+
+/*
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        System.out.println("Age=" + age);
+//        scanner.nextLine();
+//        String name = scanner.nextLine();
+//        System.out.println("Name=" + name);
+
+        double xkvadrat = Math.pow(age, 0.5);
+        System.out.println("Kvadrat=" + xkvadrat);
+
+        double koren = Math.sqrt(age);
+        System.out.println("Koren=" + koren);
+
+        boolean condition = age != 18;
+        if (!condition) { // age == 18
+            System.out.println("Все ок, можешь закупаться подиками");
+        } else {
+            System.out.println("чел, сорян, ты еще маленький");
+        }
+*/
+
+        String name = "Maxim";
+        String name2 = "Ivan";
+
+        int age = 22;
+
+    }
+
+}
